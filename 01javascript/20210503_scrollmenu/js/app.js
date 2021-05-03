fa621948a3a46ab794e559f099641b6a86a56d77
jwb117 <jwbjoo@naver.com>
2021-05-03 18:13:31 +0900

    final

--- 01javascript/20210503_scrollmenu/js/app.js
+++ 01javascript/20210503_scrollmenu/js/app.js
@@ -5,7 +5,8 @@ $(document).ready(function(){
   const twoTop = $("#v02s").offset().top;
   const threeTop = $("#v03s").offset().top;
 
-  // $(".menu > a").eq(0).on("click", function(){});
+  // $(".menu > a").eq(0).on("click", function(){}); 온다음에 클릭이벤트, 무명함수 지정형
+
   // $(".menu > a").eq(0).click(function(){
   //   $("html, body").animate({scrollTop:oneTop}, 1000);
   // });
@@ -35,8 +36,27 @@ $(document).ready(function(){
       $("html,body").animate({scrollTop:st}, 1000);
     });
 
-        $(".menu>a").click(function(){
-          $(".menu>a").removeClass("active");
-          $(this).addClass("active");
-        });
+    // $(window).scroll(function(){
+    //   let scrollY = $(window).scrollTop();
+    //   console.log(scrollY);
+    //   if(scrollY >= $(".scene").eq(0).offset().top){
+    //     $(".menu>a").removeClass("active").eq(0).addClass("active");
+    //   }
+    //   if(scrollY >= $(".scene").eq(1).offset().top){
+    //     $(".menu>a").removeClass("active").eq(1).addClass("active");
+    //   }
+    //   if(scrollY >= $(".scene").eq(2).offset().top){
+    //     $(".menu>a").removeClass("active").eq(2).addClass("active");
+    //   }
+    // });
+    $(window).scroll(function(){
+    for(i=0; i<$(".scene").length; i++){
+      let scrollY = $(window).scrollTop();
+      console.log(scrollY);
+      let st = $(".scene").eq(i).offset().top
+        if(scrollY >= st){$(".menu>a").removeClass("active").eq(i).addClass("active");
+       }
+     }
+    });
+
 });
