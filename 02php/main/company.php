4618b4a257954d0584e66b9eb87ec6f85bafb405
jwb117 <jwbjoo@naver.com>
2021-05-11 18:12:36 +0900

    final

---
+++ 02php/main/company.php
@@ -0,0 +1,16 @@
+<? $location = "company"; ?>
+
+<? include "header.php" ?>
+<div class="visual company">
+    company
+</div>
+  <div class="container">
+    <section class="content">
+      content
+    </section>
+    <aside class="aside">
+      aside
+    </aside>
+  </div>
+
+<? include "footer.php" ?>
---
+++ 02php/main/product.php
@@ -0,0 +1,16 @@
+<? $location = "product"; ?>
+
+<? include "header.php" ?>
+<div class="visual <?echo $location?>">
+    <?echo $location;?>
+</div>
+  <div class="container">
+    <section class="content">
+      content
+    </section>
+    <aside class="aside">
+      aside
+    </aside>
+  </div>
+
+<? include "footer.php" ?>
