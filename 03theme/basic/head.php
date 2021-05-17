c2cc50f8434cad8bb469ea4482a062669e006bbc
jwb117 <jwbjoo@naver.com>
2021-05-17 14:24:02 +0900

    final

--- 03theme/basic/head.php
+++ 03theme/basic/head.php
@@ -29,7 +29,7 @@ include_once(G5_LIB_PATH.'/popular.lib.php');
     <div id="hd_wrapper">
 
         <div id="logo">
-            <a href="<?php echo G5_URL ?>"><img src="<?php echo G5_IMG_URL ?>/logo.png" alt="<?php echo $config['cf_title']; ?>"></a>
+            <a href="<?php echo G5_URL ?>"><img src="<?php echo G5_THEME_IMG_URL ?>/logo2.png" alt="<?php echo $config['cf_title']; ?>"></a>
         </div>
 
         <ul class="hd_login">
@@ -51,7 +51,7 @@ include_once(G5_LIB_PATH.'/popular.lib.php');
         <h2>메인메뉴</h2>
         <div class="gnb_wrap">
             <ul id="gnb_1dul">
-                <li class="gnb_1dli gnb_mnal"><button type="button" class="gnb_menu_btn" title="전체메뉴"><i class="fa fa-bars" aria-hidden="true"></i><span class="sound_only">전체메뉴열기</span></button></li>
+
                 <?php
 				$menu_datas = get_menu_db(0, true);
 				$gnb_zindex = 999; // gnb_1dli z-index 값 설정용
@@ -88,56 +88,8 @@ include_once(G5_LIB_PATH.'/popular.lib.php');
                     <li class="gnb_empty">메뉴 준비 중입니다.<?php if ($is_admin) { ?> <a href="<?php echo G5_ADMIN_URL; ?>/menu_list.php">관리자모드 &gt; 환경설정 &gt; 메뉴설정</a>에서 설정하실 수 있습니다.<?php } ?></li>
                 <?php } ?>
             </ul>
-            <div id="gnb_all">
-                <h2>전체메뉴</h2>
-                <ul class="gnb_al_ul">
-                    <?php
-
-                    $i = 0;
-                    foreach( $menu_datas as $row ){
-                    ?>
-                    <li class="gnb_al_li">
-                        <a href="<?php echo $row['me_link']; ?>" target="_<?php echo $row['me_target']; ?>" class="gnb_al_a"><?php echo $row['me_name'] ?></a>
-                        <?php
-                        $k = 0;
-                        foreach( (array) $row['sub'] as $row2 ){
-                            if($k == 0)
-                                echo '<ul>'.PHP_EOL;
-                        ?>
-                            <li><a href="<?php echo $row2['me_link']; ?>" target="_<?php echo $row2['me_target']; ?>"><?php echo $row2['me_name'] ?></a></li>
-                        <?php
-                        $k++;
-                        }   //end foreach $row2
-
-                        if($k > 0)
-                            echo '</ul>'.PHP_EOL;
-                        ?>
-                    </li>
-                    <?php
-                    $i++;
-                    }   //end foreach $row
-
-                    if ($i == 0) {  ?>
-                        <li class="gnb_empty">메뉴 준비 중입니다.<?php if ($is_admin) { ?> <br><a href="<?php echo G5_ADMIN_URL; ?>/menu_list.php">관리자모드 &gt; 환경설정 &gt; 메뉴설정</a>에서 설정하실 수 있습니다.<?php } ?></li>
-                    <?php } ?>
-                </ul>
-                <button type="button" class="gnb_close_btn"><i class="fa fa-times" aria-hidden="true"></i></button>
-            </div>
-            <div id="gnb_all_bg"></div>
-        </div>
     </nav>
-    <script>
 
-    $(function(){
-        $(".gnb_menu_btn").click(function(){
-            $("#gnb_all, #gnb_all_bg").show();
-        });
-        $(".gnb_close_btn, #gnb_all_bg").click(function(){
-            $("#gnb_all, #gnb_all_bg").hide();
-        });
-    });
-
-    </script>
 </div>
 <!-- } 상단 끝 -->
 
@@ -180,11 +132,44 @@ include_once(G5_LIB_PATH.'/popular.lib.php');
 
 </style>
 
+<? if(!defined('_INDEX_')){?>
+ <div class="subVisual">
+  <div class="subImg" id="page_title">
+    <div class="title">
+      <h2 class="loc1D"></h2>
+          <div class="txt"></div>
+    </div>
+  </div>
+</div>
+
+
+
+<script>
+  window.onload = function(){
+    const menuDep = $(".loc1D").html(); //get으로 데이터를 가져와서 const저장소에 저장함
+    console.log("현재위치 : " + menuDep);
+    if(menuDep == "회사소개"){ $(".txt").html(
+      "저희 회사 홈페이지를 찾아주셔서 감사합니다.")
+  }
+    if(menuDep == "제품소개"){ $(".txt").html(
+      "저희 회사의 제품을 소개합니다.")
+  }
+     if(menuDep == "커뮤니티"){ $(".txt").html(
+       "자유롭게 커뮤니티를 하는 공간입니다.")
+  }
+};
+</script>
+
+<? } ?>
 
 <!-- 콘텐츠 시작 { -->
 <div id="wrapper">
     <div id="container_wr"> <!--1200px-->
 
-    
+
     <div id="container" <? if(defined("_INDEX_")){?> style="width:1200px" <?}?>> <!--930px-->
-        <?php if (!defined("_INDEX_")) { ?><h2 id="container_title"><span title="<?php echo get_text($g5['title']); ?>"><?php echo get_head_title($g5['title']); ?></span></h2><?php }
+        <?php if (!defined("_INDEX_")) { ?>
+
+          <!-- location -->
+          <div>HOME > <span class="loc1D"></span> > <?php echo get_text($g5['title']); ?> </div>
+          <h2 id="container_title"><span title="<?php echo get_text($g5['title']); ?>"><?php echo get_head_title($g5['title']); ?></span></h2><?php }
